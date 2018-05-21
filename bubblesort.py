ab7d80a7b111c4adb45b23fc58f26e7dfb7a6a63
aduil <aduil526@outlook.com>
2018-05-21 11:02:23 +0800

    Sort

---
+++ bubblesort.py
@@ -0,0 +1,12 @@
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp=alist[i]
+                alist[i]=alist[i+1]
+                alist[i+1]=temp
+
+
+testlist=[45,54,56,52,632,562,326,596]
+bubbleSort(testlist)
+print(testlist)
