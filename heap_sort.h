c31a6e79565002142b46cce4da63c1082c33756a
aduil <tt19921101@163.com>
2019-12-26 01:24:53 -0800

    堆排序

---
+++ heap_sort.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+
+using std::swap;
+
+void max_heapify(int arr[],int start, int end){
+    int dad=start;
+    int son=dad*2+1;
+    while(son<=end){
+        if(son+1<end&&arr[son]<arr[son+1])
+            son++;
+        if(arr[dad]>arr[son])
+            return;
+        else{
+            swap(arr[dad], arr[son]);
+            dad=son;
+            son=dad*2+1;
+        }
+    }
+}
+
+void heap_sort(int arr[], int len){
+    for(int i=len/2-1;i>=0;i--)
+        max_heapify(arr, i, len-1);
+    for(int i=len-1; i>0; i--){
+        swap(arr[0], arr[i]);
+        max_heapify(arr, 0, i-1);
+    }
+}
