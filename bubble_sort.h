fe388af2e56d08adc9c01f843a1fa5b261a15d3f
aduil <tt19921101@163.com>
2019-12-26 02:56:06 -0800

    快排

---
+++ bubble_sort.h
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void bubble_sort(int arr[], int length){
+    if(arr==NULL||length<=1)
+        return;
+    bool flag=false;
+    for(int i=length-1; i>0; i--){
+        flag=false;
+        for(int j=0;j<i;j++){
+            if(arr[j]>arr[j+1]){
+                std::swap(arr[j], arr[j+1]);
+                flag=true;
+            }
+        }
+        if(flag==false)
+            return;
+    }
+}
